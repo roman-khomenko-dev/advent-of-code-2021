fb1db7f0c09e5beaeb03addd095cee9f4db2a8fc
roman-khomenko-dev <roman.khomenko@litslink.com>
2023-02-08 19:01:54 +0200

    Implement solution for the day 1 part#2

--- lib/day_1/sonar_sweep.ex
+++ lib/day_1/sonar_sweep.ex
@@ -4,20 +4,37 @@ defmodule AdventOfCode2021.SonarSweep do
   Day 1: Sonar Sweep
   """
 
-  @puzzle_input []
-
-  def find_keys do
+  def find_keys(type = 1) do
     {_, puzzle} = get_puzzle()
-    data = %{keys:  0, previous: nil}
+    data = %{keys: 0, previous: nil}
 
     result = Enum.reduce(puzzle, data, fn value, acc ->
-      proceed_key(acc, value)
+      proceed_key(1, acc, value)
     end)
     inspect(result.keys)
   end
 
-  defp proceed_key(data, value) do
+  def find_keys(type = 2) do
+    {_, puzzle} = get_puzzle()
+    data = %{keys: 0, previous: nil}
+    chunked_puzzle = Enum.chunk_every(puzzle, 3, 1, :discard)
+
+    result = Enum.reduce(chunked_puzzle, data, fn value, acc ->
+      proceed_key(2, acc, value)
+    end)
+    inspect(result.keys)
+  end
+
+  defp proceed_key(type = 1, data, value) do
     if value > data.previous, do: %{data | keys: data.keys + 1, previous: value}, else: %{data | previous: value}
+  end
+
+  defp proceed_key(type = 2, data = %{previous: nil}, value) do
+    %{data | previous: value}
+  end
+
+  defp proceed_key(type = 2, data, value) do
+    if Enum.sum(value) > Enum.sum(data.previous), do: %{data | keys: data.keys + 1, previous: value}, else: %{data | previous: value}
   end
 
   defp get_puzzle do
