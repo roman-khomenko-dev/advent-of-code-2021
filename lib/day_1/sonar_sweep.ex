4d449faec2f4149dfaaa0a386d42f1ee6e741e65
roman-khomenko-dev <roman.khomenko@litslink.com>
2023-02-13 16:01:01 +0200

    Implement solution of the Day 2 - Dive, add credo

--- lib/day_1/sonar_sweep.ex
+++ lib/day_1/sonar_sweep.ex
@@ -3,37 +3,37 @@ defmodule AdventOfCode2021.SonarSweep do
   Advent of Code 2021
   Day 1: Sonar Sweep
   """
+  @data %{keys: 0, previous: nil}
 
-  def find_keys(type = 1) do
+  @spec find_keys(1 | 2) :: binary
+  def find_keys(_type = 1) do
     {_, puzzle} = get_puzzle()
-    data = %{keys: 0, previous: nil}
 
-    result = Enum.reduce(puzzle, data, fn value, acc ->
+    result = Enum.reduce(puzzle, @data, fn value, acc ->
       proceed_key(1, acc, value)
     end)
     inspect(result.keys)
   end
 
-  def find_keys(type = 2) do
+  def find_keys(_type = 2) do
     {_, puzzle} = get_puzzle()
-    data = %{keys: 0, previous: nil}
     chunked_puzzle = Enum.chunk_every(puzzle, 3, 1, :discard)
 
-    result = Enum.reduce(chunked_puzzle, data, fn value, acc ->
+    result = Enum.reduce(chunked_puzzle, @data, fn value, acc ->
       proceed_key(2, acc, value)
     end)
     inspect(result.keys)
   end
 
-  defp proceed_key(type = 1, data, value) do
+  defp proceed_key(_type = 1, data, value) do
     if value > data.previous, do: %{data | keys: data.keys + 1, previous: value}, else: %{data | previous: value}
   end
 
-  defp proceed_key(type = 2, data = %{previous: nil}, value) do
+  defp proceed_key(_type = 2, data = %{previous: nil}, value) do
     %{data | previous: value}
   end
 
-  defp proceed_key(type = 2, data, value) do
+  defp proceed_key(_type = 2, data, value) do
     if Enum.sum(value) > Enum.sum(data.previous), do: %{data | keys: data.keys + 1, previous: value}, else: %{data | previous: value}
   end
 
